cf22fdeb2635dc65ed6c4bf6f73302980b30e702
Mark Grant <merely.semantics@gmail.com>
2012-02-24 10:08:01 -0600

    Working Connection spec/class

--- src/server/connection.rb
+++ src/server/connection.rb
@@ -1,24 +1,39 @@
+require File.join(File.dirname(__FILE__), '../javattt/ttt.jar')
+require File.join(File.dirname(__FILE__), 'httpgame')
+require File.join(File.dirname(__FILE__), 'HTTPUI')
 
 
-class Connection < Sinatra::Request
-	@@id_counter = 1
+class Connection
+	@@id_counter = 0
 	@@connections = {}
 
-	def id
-		session[:id]
-	end
-
-	def initialize id
-		if @@connections[id]
-			@connections[id]
-		else
-			session[:id] = @@id_counter
-			@@connections[id] = self
-			@id_counter += 1
-		end
-	end
+	attr_accessor :id, :game
 
 	def disconnect
 		@@connections.delete id
+	end
+
+	def self.register
+		@@id_counter += 1
+		@@connections[@@id_counter] = Connection.new
+		@@id_counter
+	end
+
+	def initialize
+		@game = HTTPGame.new
+		@game.ui = HTTPUI.new
+	end
+
+	def self.create
+		Connection[@@id_counter]
+	end
+
+	def self.[](id)
+		@@connections[id]
+	end
+
+	def self.clear_all
+		@@id_counter = 0
+		@@connections = {}
 	end
 end
---
+++ test/server/connection_spec.rb
@@ -0,0 +1,37 @@
+require '../../src/server/connection'
+
+
+describe Connection, "#initialize" do
+	after :each do
+		Connection.clear_all
+	end
+
+	it "creates a new session" do
+		id = Connection.register
+		conn = Connection[id]
+
+		id.should == 1
+		Connection[id].class.should == Connection
+	end
+
+	it "creates increasing session id's" do
+		id1 = Connection.register
+		id2 = Connection.register
+
+		id1.should == 1
+		id2.should == 2
+
+		Connection[id1].class.should == Connection
+		Connection[id2].class.should == Connection
+
+		Connection[id1].should_not == Connection[id2]
+	end
+
+	it "creates a game for each connection" do
+		id = Connection.register
+		game = Connection[id].game
+
+		game.class.should == HTTPGame
+		game.stage.toString.should == "newGame"
+	end
+end
