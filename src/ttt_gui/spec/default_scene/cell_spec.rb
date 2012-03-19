a57634b99ab2f7f9da4e61891d0db101d723db8b
Mark Grant <merely.semantics@gmail.com>
2012-03-19 15:28:27 -0500

    Removed cell.rb in spec

--- src/ttt_gui/spec/default_scene/cell_spec.rb
+++
@@ -1,3 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
-require File.expand_path(File.dirname(__FILE__) + "/../../default_scene/players/cell")
-#require './default_scene/players/cell'
